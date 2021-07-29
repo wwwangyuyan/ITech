e2784119521705c50e25469684d11932bf6ffd53
Yuyan Wang <2492467w@student.gla.ac.uk>
2021-07-30 00:45:11 +0100

    complete the chapter 7 but have not tested.

--- rango/views.py
+++ rango/views.py
@@ -4,10 +4,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 
 def index(request):
-
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
@@ -15,7 +16,6 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-
 
     return render(request, 'rango/index.html', context=context_dict)
 
@@ -28,7 +28,6 @@ def about(request):
 
 
 def show_category(request, category_name_slug):
-
     context_dict = {}
     try:
 
@@ -43,3 +42,15 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+    if form.is_valid():
+        form.save(commit=True)
+        return redirect('/rango/')
+    else:
+        print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})

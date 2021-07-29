88f372f292c820a629cb602120c78dab01b22e1e
Yuyan Wang <2492467w@student.gla.ac.uk>
2021-07-29 03:25:38 +0100

    complete the chapter 5 but have not tested

--- rango/views.py
+++ rango/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-  context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-  #chapter3:return HttpResponse("Rango says hey there partner!""<a href='/rango/about/'>About</a>")
-  return render(request, 'rango/index.html', context=context_dict)
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # chapter3:return HttpResponse("Rango says hey there partner!""<a href='/rango/about/'>About</a>")
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
-  # chapter4.3：return HttpResponse("Rango says here is the about page.""<a href='/rango/'>Index</a>")
+    # chapter4.3：return HttpResponse("Rango says here is the about page.""<a href='/rango/'>Index</a>")
 
- context_dict = {'boldmessage': 'This tutorial has been put together by yuyan wang'}
- return render(request, 'rango/about.html', context=context_dict)
+    context_dict = {'boldmessage': 'This tutorial has been put together by yuyan wang'}
+    return render(request, 'rango/about.html', context=context_dict)

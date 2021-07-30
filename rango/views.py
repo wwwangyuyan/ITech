4cef7bf746a7895b01b06e46046faefadd3533da
Yuyan Wang <2492467w@student.gla.ac.uk>
2021-07-30 02:09:00 +0100

    complete the chapter 8 but not tested

--- rango/views.py
+++ rango/views.py
@@ -50,11 +50,11 @@ def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
         form = CategoryForm(request.POST)
-    if form.is_valid():
-        form.save(commit=True)
-        return redirect('/rango/')
-    else:
-        print(form.errors)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
 
